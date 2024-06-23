07066dcea88e6f9c3bb219ac438067b67d46f748
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-06-23 12:31:23 -0400

    MC-0005: Added Welcome message showing user's first name. Shows today`s date on Home page's Info card. Made cards clickable (Calendar, Reminders, Tips, Settings). Added Activities: Calendar, Reminders, Tips, Settings. All redirect to Home page when clicking on Back Icon.

--- app/src/main/java/com/ericadiogo/mycycle/HomeActivity.java
+++ app/src/main/java/com/ericadiogo/mycycle/HomeActivity.java
@@ -2,20 +2,32 @@ package com.ericadiogo.mycycle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
-    private TextView usergreeting;
+    private TextView usergreeting,todayDate;
+    private CardView cardCalendar,cardReminder,cardTips,cardSettings;
+    private FirebaseUser loggedUser;
+    private DatabaseReference reference;
+    private String loggedUserId;
     private FirebaseAuth mAuth;
 
     @Override
@@ -23,8 +35,68 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        todayDate = findViewById(R.id.todayDate);
         usergreeting = findViewById(R.id.userGreeting);
+        mAuth = FirebaseAuth.getInstance();
+        loggedUserId = mAuth.getUid();
+        todayDate.setText(showDate());
+        cardCalendar = findViewById(R.id.cardCalendar);
+        cardReminder = findViewById(R.id.cardReminder);
+        cardTips = findViewById(R.id.cardTips);
+        cardSettings = findViewById(R.id.cardSettings);
+        reference = FirebaseDatabase.getInstance().getReference("users");
 
+        reference.child(loggedUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserModel userModel = snapshot.getValue(UserModel.class);
+                if(userModel != null){
+                    String fname = userModel.getFirstName();
+                    usergreeting.setText("Hello, " + fname + "!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        cardCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,CalendarActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,RemindersActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardTips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,TipsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,SettingsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
@@ -32,9 +104,15 @@ public class HomeActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if(currentUser == null){
-            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+           Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+           startActivity(intent);
+           finish();
         }
     }
+
+    protected String showDate(){
+        String date = new SimpleDateFormat("EEE, MMM dd", Locale.getDefault()).format(new Date());
+        return date;
+    }
 }
+
--- app/src/main/java/com/ericadiogo/mycycle/UserModel.java
+++ app/src/main/java/com/ericadiogo/mycycle/UserModel.java
@@ -11,6 +11,10 @@ public class UserModel {
         this.password = password;
     }
 
+    public UserModel(){
+
+    }
+
     public String getId() { return id; }
 
     public void setId(String id) { this.id = id; }
