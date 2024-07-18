6e3e1768f945cf1b5ff9a93016fd5062f9cc9002
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-07-18 12:14:02 -0400

    MC-0009: Showing selected date on daily info alert dialog.

--- app/src/main/java/com/ericadiogo/mycycle/CalendarActivity.java
+++ app/src/main/java/com/ericadiogo/mycycle/CalendarActivity.java
@@ -3,16 +3,26 @@ package com.ericadiogo.mycycle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,6 +32,11 @@ public class CalendarActivity extends AppCompatActivity {
     private LinearLayout calendarBack;
     private CalendarView calView;
     private TextView pickedDate;
+    private ImageView addInfobtn;
+    private DatabaseReference reference;
+    private String loggedUserId, dateSel;
+    private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,7 @@ public class CalendarActivity extends AppCompatActivity {
         calendarBack = findViewById(R.id.calendarBack);
         calView = findViewById(R.id.calView);
         pickedDate = findViewById(R.id.pickedDate);
+        addInfobtn = findViewById(R.id.addInfobtn);
 
         calendarBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,8 +62,66 @@ public class CalendarActivity extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 String date = (month+1) + "/" + day + "/" + year;
+                dateSel = date;
                 pickedDate.setText(date);
             }
         });
+
+        addInfobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogDailyInfo();
+            }
+        });
+    }
+
+    private void showDialogDailyInfo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(CalendarActivity.this);
+        View view1 = getLayoutInflater().inflate(R.layout.daily_info,null);
+        Button btnSaveInfo,btnCancelInfo;
+        TextView selDate;
+
+        btnSaveInfo = view1.findViewById(R.id.btnSaveInfo);
+        btnCancelInfo = view1.findViewById(R.id.btnCancelInfo);
+        selDate = view1.findViewById(R.id.selDate);
+        selDate.setText(dateSel);
+        builder.setView(view1);
+
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(true);
+        dialog.show();
+
+        /* btnSaveInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference.child(loggedUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserModel userModel = snapshot.getValue(UserModel.class);
+                        if(userModel != null){
+                            int plength = userModel.getpLength();
+                            int plengthnew = Integer.valueOf(edtChangePeriod.getText().toString());
+                            if(plength != plengthnew){
+                                reference.child(loggedUserId).child("pLength").setValue(plengthnew);
+                                txtPeriod.setText("Period length: " + plengthnew + " days");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        }); */
     }
 }
