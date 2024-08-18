7b97b82e223dbc23365f56a3649a3f64268e8433
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-08-17 20:00:58 -0400

    MC-0014: Added xmls for reminders activity.

--- app/src/main/java/com/ericadiogo/mycycle/RemindersActivity.java
+++ app/src/main/java/com/ericadiogo/mycycle/RemindersActivity.java
@@ -4,28 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-
 public class RemindersActivity extends AppCompatActivity {
     private LinearLayout remindersBack;
-    private CardView cardAddReminder;
+    private CardView addReminder;
     private RecyclerView recViewReminder;
 
     @Override
@@ -35,7 +25,7 @@ public class RemindersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reminders);
 
         remindersBack = findViewById(R.id.remindersBack);
-        cardAddReminder = findViewById(R.id.cardAddReminder);
+        addReminder = findViewById(R.id.addReminder);
         recViewReminder = findViewById(R.id.recViewReminder);
 
         recViewReminder.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
@@ -50,7 +40,7 @@ public class RemindersActivity extends AppCompatActivity {
             }
         });
 
-        cardAddReminder.setOnClickListener(new View.OnClickListener() {
+        addReminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showReminderDialog();
@@ -61,11 +51,10 @@ public class RemindersActivity extends AppCompatActivity {
     private void showReminderDialog() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-
-        View p_layout = getLayoutInflater().inflate(R.layout.newreminder_dialog, null);
-        final EditText edtWeight = p_layout.findViewById(R.id.edtWeight);
-        Button btnSave = p_layout.findViewById(R.id.btnSave8);
-        Button btnCancel = p_layout.findViewById(R.id.btnCancel8);
+        View p_layout = getLayoutInflater().inflate(R.layout.reminder_dialog, null);
+        //final EditText edtWeight = p_layout.findViewById(R.id.edtWeight);
+        Button btnSave = p_layout.findViewById(R.id.btnSave10);
+        Button btnCancel = p_layout.findViewById(R.id.btnCancel10);
 
         builder.setView(p_layout);
         AlertDialog dialog = builder.create();
@@ -75,7 +64,7 @@ public class RemindersActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                reference.child(loggedUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+            /*    reference.child(loggedUserId).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         UserModel userModel = snapshot.getValue(UserModel.class);
@@ -102,7 +91,7 @@ public class RemindersActivity extends AppCompatActivity {
 
                     }
                 });
-                dialog.dismiss();
+                dialog.dismiss(); */
             }
         });
 
