3e8cbceeacc4fd4e812a7b5578801fd5c6c40ac5
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-08-17 21:31:05 -0400

    MC-0014: Added reminder worker.

---
+++ app/src/main/java/com/ericadiogo/mycycle/ReminderWorker.java
@@ -0,0 +1,30 @@
+package com.ericadiogo.mycycle;
+
+import android.content.Context;
+
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class ReminderWorker extends Worker {
+
+    private final Context context;
+
+    public ReminderWorker(Context context, WorkerParameters params) {
+        super(context, params);
+        this.context = context;
+    }
+
+    @Override
+    public Result doWork() {
+        Data inputData = getInputData();
+        String title = inputData.getString("Title");
+        String message = inputData.getString("Message");
+
+        if (title != null && message != null) {
+            new NotificationHelper(context).createNotification(title, message);
+        }
+
+        return Result.success();
+    }
+}
