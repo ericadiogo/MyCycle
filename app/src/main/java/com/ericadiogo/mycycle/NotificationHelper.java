f1000d25c9afcb868764a7e26564787808d50fbe
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-08-17 21:31:58 -0400

    MC-0014: Added notification helper.

---
+++ app/src/main/java/com/ericadiogo/mycycle/NotificationHelper.java
@@ -0,0 +1,71 @@
+package com.ericadiogo.mycycle;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+public class NotificationHelper {
+
+    private final Context context;
+    private final String CHANNEL_ID = "REMINDER_NOTIFICATIONS";
+    private final int NOTIFICATION_ID;
+
+    public NotificationHelper(Context context) {
+        this.context = context;
+        String timeString = String.valueOf(System.currentTimeMillis());
+        this.NOTIFICATION_ID = Integer.parseInt(timeString.substring(timeString.length() - 5));
+    }
+
+    public void createNotification(String title, String message) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+
+        createNotificationChannel();
+
+        Intent intent = new Intent(context, RemindersActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 0, intent, PendingIntent.FLAG_IMMUTABLE
+        );
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_notifications_active_24)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.baseline_notifications_active_24))
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Reminder Notifications",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("Reminder channel to display all user scheduled reminders");
+
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+}
